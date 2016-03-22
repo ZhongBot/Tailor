37aeba8e0387679476d2ed5153d0eafa837cc430
ZhongBot <zhong.botian@gmail.com>
2016-03-22 15:11:28 -0400

    read sizing data from csv and store in dict

--- scrapper/scrapper/model/sizing.py
+++ scrapper/scrapper/model/sizing.py
@@ -1,22 +1,28 @@
+import csv
+
+
 class sizing(object):
-    default = '_not_available_'
-    
-    type_of_clothing = default
     """
     {
-        type_of_measurement: {
-            'size':('lowerbound', 'upperbound')
+        "h&m" : {
+            "s" : (30, 32),
+            "m" : (32, 34)
+        },
+        "uniqlo" : {
+            "s" : (30, 32),
+            "m" : (32, 34)
         }
-    } 
-    ALL MEASURED IN CM
-    Example: {"ARM":{"XS":(81,81)}}
+    }
     """
-    measurements = {}
+    sizing_dict = {}
+    with open("sizing.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if len(row) < 4:
+                continue
 
-    def to_dict(self):
-        _dict = {
-            'type_of_clothing': self.type_of_clothing,
-            'sizing': self.measurements
-        }
-        return _dict
-    
+            # row = [brand_name, size, low_bound, upper_bound]
+            if row[0] in sizing_dict:
+                sizing_dict[row[0]][row[1]] = (int(row[2]), int(row[3]))
+            else:
+                sizing_dict[row[0]] = {row[1]: (int(row[2]), int(row[3]))}
